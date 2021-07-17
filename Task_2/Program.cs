72b0fefe6cb5f2286fa44007d2245ed67d230930
Abdul Moiz Siraj <abdul.moiz@astera.com>
2021-07-18 00:43:53 +0500

    programs changes after factory

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,16 +3,31 @@
 namespace Task_2 {
     internal class Program {
         private static void Main(string[] args) {
+
             Employee empOne = new("Moiz", 23, "Software Dev");
-            Console.WriteLine("Serialization Start");
-            XMLSerializer xmlSerializer = new(typeof(Employee), empOne, "./Outputs.xml");
-            SerializeCommand serializeCommand = new(xmlSerializer);
+            SerializerFactory serializeFactory = new();
+            SerializeCommand serializeCommand = serializeFactory.getSerializer("JSON", typeof(Employee), empOne);
             serializeCommand.Execute();
-            Console.WriteLine("Deserialization Start");
-            XMLDeserializer xmlDeserialiser = new(typeof(Employee), "./Outputs.xml");
+            
+            /*Console.WriteLine("Deserialization Start");
+            XMLDeserializer xmlDeserialiser = new(typeof(Employee), XMLFilePath);
             DeserializeCommand deserializeCommand = new(xmlDeserialiser);
             Employee empTwo = (Employee)deserializeCommand.Execute();
-            Console.WriteLine(empTwo.Designation);
+            Console.WriteLine(empTwo.Name);
+
+            Console.WriteLine("JSON");
+            Console.WriteLine("Serialization Start");
+            Employee empThree = new("Haris", 22, "Web Dev");
+            JSONSerializer jsonSerializer = new(empThree, JSONFilePath);
+            serializeCommand = new(jsonSerializer);
+            serializeCommand.Execute();
+
+            Console.WriteLine("Deserialization Start");
+            JSONDeserializer jsonDeserializer = new(typeof(Employee), JSONFilePath);
+            deserializeCommand = new(jsonDeserializer);
+            Employee empFour = (Employee)deserializeCommand.Execute();
+            Console.WriteLine(empFour.Name);*/
+
         }
     }
 }

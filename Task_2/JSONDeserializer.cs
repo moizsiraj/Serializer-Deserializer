c30d0975476b9797034bfd0902ab8f745c92e60c
Abdul Moiz Siraj <abdul.moiz@astera.com>
2021-07-18 00:42:21 +0500

    json deserializer added

--- Task_2/JSONDeserializer.cs
+++ Task_2/JSONDeserializer.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Task_2 {
     internal class JSONDeserializer : IDeserializer {
+
+        private Type _ObjectType;
+
+        private string _FilePath;
+
+        private JsonSerializer _JsonDeserializer;
+
+        public JSONDeserializer(Type type, string filePath) {
+            _ObjectType = type;
+            _FilePath = filePath;
+            _JsonDeserializer = new JsonSerializer();
+        }
+
         public object Deserialize() {
-            throw new NotImplementedException();
+            JObject deserializedObject = null;
+
+            if (File.Exists(_FilePath)) {
+                StreamReader streamReader = new StreamReader(_FilePath);
+                JsonReader jsonReader = new JsonTextReader(streamReader);
+                deserializedObject = _JsonDeserializer.Deserialize(jsonReader) as JObject;
+                jsonReader.Close();
+                streamReader.Close();
+            }
+
+            return deserializedObject.ToObject(_ObjectType);
         }
     }
 }

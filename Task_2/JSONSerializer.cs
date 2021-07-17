2f97dda5df63cb7ed890c888ba60eb59174ec4c2
Abdul Moiz Siraj <abdul.moiz@astera.com>
2021-07-18 00:42:36 +0500

    json serializer added

--- Task_2/JSONSerializer.cs
+++ Task_2/JSONSerializer.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Task_2 {
     internal class JSONSerializer : ISerializer {
+
+        private object _ObjectData;
+
+        private string _FilePath;
+
+        private JsonSerializer _JSONSerializer;
+
+        public JSONSerializer(object objectData, string filePath) {
+            _ObjectData = objectData;
+            _FilePath = filePath;
+            _JSONSerializer = new JsonSerializer();
+        }
+
         void ISerializer.Serialize() {
-            throw new NotImplementedException();
+            if (File.Exists(_FilePath)) {
+                File.Delete(_FilePath);
+            }
+            StreamWriter streamWriter = new(_FilePath);
+            JsonWriter jsonWriter = new JsonTextWriter(streamWriter);
+            _JSONSerializer.Serialize(jsonWriter, _ObjectData);
+            jsonWriter.Close();
+            streamWriter.Close();
         }
     }
 }
